fe3e8c52a991bc40a99fcc2a319b1f2c5e1596be
kristof <kristof.frelih@gmail.com>
2025-07-08 22:40:15 +0200

    corrected ruter arhitecture,added features. User part complete

--- api/index.php
+++ api/index.php
@@ -10,30 +10,76 @@ $parts = explode("/", $path);
 
 
 $resource = $parts[3];
-
-if ($resource != "getAllStudents") {
-
-    http_response_code(404);
-    exit;
-}
-
+$order = $parts[4];
 
 $user = new UserGateway($database);
-
-
 $JwtCtrl = new Jwt($_ENV["SECRET_KEY"]);
-
 $auth = new Auth($user, $JwtCtrl);
+$IDuser = $auth->authenticateJWTToken();
+if (!$IDuser) {exit;}
 
-if (!$auth->authenticateJWTToken()) {
-    echo $auth->getUserIdFromToken();
-    //echo $auth->getUserIdFromToken();
-    exit;
+switch($resource){
+    case "users":
+        switch($order){
+            case "getAllUsers":
+                getAllUsers($database);
+                break;
+            case "updateUser":
+                updateUser($database,$IDuser);    
+                break;
+            case "deleteUser":
+                deleteUser($database,$IDuser);
+                break;
+        }
+        break;
+    case "cars":
+            successRateBrandModelDate($database);
+        break;
+    default:
+        http_response_code(404);
+        exit;
 }
 
+function getAllUsers($database)
+{
+    /*$user = new UserGateway($database);
+    $JwtCtrl = new Jwt($_ENV["SECRET_KEY"]);
+    $auth = new Auth($user, $JwtCtrl);*/
+    $gateway = new UserGateway($database);
+    $controller = new UserController($gateway);
+    $controller->processRequest($_SERVER['REQUEST_METHOD']);
+   
+}
+function updateUser($database,$IDuser)
+{
+    /*$user = new UserGateway($database);
+    $JwtCtrl = new Jwt($_ENV["SECRET_KEY"]);
+    $auth = new Auth($user, $JwtCtrl);*/
+    $gateway = new UserGateway($database);
+    $controller = new UserController($gateway);
+    //$IDuser = $auth->authenticateJWTToken();
+    $controller->processRequest($_SERVER['REQUEST_METHOD'],$IDuser);
+}
 
-
-$gateway = new StudentGateway($database);
-$controller = new StudentController($gateway);
-
-$controller->processRequest($_SERVER['REQUEST_METHOD']);
+function deleteUser($database,$IDuser)
+{
+    /*$user = new UserGateway($database);
+    $JwtCtrl = new Jwt($_ENV["SECRET_KEY"]);
+    $auth = new Auth($user, $JwtCtrl);*/
+    $gateway = new UserGateway($database);
+    $controller = new UserController($gateway);
+    //$IDuser = $auth->authenticateJWTToken();
+    $controller->processRequest($_SERVER['REQUEST_METHOD'],$IDuser);
+}
+function CarByVIN($database,$VIN)
+{
+    $gateway = new CarGateway($database);
+    $controller = new CarController($gateway);
+    $controller->processRequest($_SERVER['REQUEST_METHOD']);
+}
+function successRateBrandModelDate($database)
+{
+    $gateway = new CarGateway($database);
+    $controller = new CarController($gateway);
+    $controller->processRequest($_SERVER['REQUEST_METHOD']);
+}
