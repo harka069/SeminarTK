6340747e72196180f23efcb618c8c80b2ec53e33
kristof <kristof.frelih@gmail.com>
2025-07-10 10:13:38 +0200

    added MotStatByQurey

--- src/CarGateway.php
+++ src/CarGateway.php
@@ -9,16 +9,6 @@ class CarGateway
     {
         $this->conn = $database->getConnection();
     }
-    public function MotStatByVIN(int $id): array | false
-    {
-        $stmt = $this->conn->prepare($sql);
-        
-        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
-        
-        $stmt->execute();
-        
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
 
     public function CarByVIN(string $VIN)
     {
@@ -69,25 +59,24 @@ class CarGateway
     }
     public function MotStatByQuerry(string $startDate, string $endDate, string $brand, string $model, int $max_km,int $min_km,string $fuel): array|false
     {
-            $sql = "SELECT 
-                a.VIN,
-                a.prevozeni_kilometri,
-                a.datum_prve_registracije,
-                z.znamka_ime AS brand,
-                m.model_ime AS model,
-                g.gorivo_tip AS gorivo
-                
-            FROM avtomobili a
+           
+        
+             $sql =   "SELECT
+            COUNT(*) AS total_count,
+            COUNT(CASE WHEN r.TEHNICNI_PREGLED_STATUS IN ('brezhiben', 'pogojno brezhiben') THEN 1 END) AS brezhiben_count,
+            COUNT(CASE WHEN r.TEHNICNI_PREGLED_STATUS IN ('ni brezhiben', 'ni brezhiben - kriticna napaka') THEN 1 END) AS ne_brezhiben_count
+        FROM avtomobili a
             JOIN znamke z ON a.znamka_id = z.znamka_id
             JOIN modeli m ON a.modeli_id = m.model_id
             JOIN gorivo g ON a.gorivo_id = g.gorivo_id
-            WHERE z.znamka_ime = :brand
-              AND m.model_ime = :model
-              AND a.datum_prve_registracije BETWEEN :start_date AND :end_date
-              AND g.gorivo_tip = :fuel
-              AND a.prevozeni_kilometri BETWEEN :min_km AND :max_km";
-             
+            JOIN rezultatiTP r ON a.VIN = r.VIN
+        WHERE z.znamka_ime = :brand
+            AND m.model_ime = :model
+            AND a.datum_prve_registracije BETWEEN :start_date AND :end_date
+            AND g.gorivo_tip = :fuel
+            AND a.prevozeni_kilometri BETWEEN :min_km AND :max_km";
 
+                
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(":brand", $brand);
         $stmt->bindValue(":model", $model);
